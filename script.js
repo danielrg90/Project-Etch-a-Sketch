28e0b020932d8e8d6da82f146e81ff54bb19a68e
Daniel R Gonzalez <108524641+danielrg90@users.noreply.github.com>
2025-04-27 13:05:38 -0700

    create function to create grid add eventlistener to change square color upon entering

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const container = document.querySelector("#container");  
-    for (let i = 0; i < 64; i++) {
-        const div = document.createElement("div"); 
-        div.classList.add("newDiv"); 
-        container.appendChild(div); 
+const container = document.querySelector(".container");
+
+function createGrid() { 
+    let size = 800; 
+    let amountOfBoxes = 16; 
+    for (let i = 0; i < 256; i++) { 
+        const grid = document.createElement("div"); 
+        grid.classList.add("boxes"); 
+        grid.style.width = size/amountOfBoxes + "px"; 
+        grid.style.height = size/amountOfBoxes + "px"; 
+        container.appendChild(grid); 
     }
+}
+
+createGrid();
+
+const boxes = document.querySelectorAll(".boxes"); 
+
+function colorGrid() {
+    boxes.forEach(box => {
+        box.addEventListener("mouseenter", () => {
+            box.style.backgroundColor = "purple"; 
+        }); 
+    });
+}; 
+
+colorGrid(); 
